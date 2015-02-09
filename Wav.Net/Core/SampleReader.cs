3e588ad8d46b2c66fa4ac911b34ba97ef6d2f648
Sam <s.kamber8@gmail.com>
2015-02-09 13:11:42 +0000

    Remove temp debug code

--- Wav.Net/Core/SampleReader.cs
+++ Wav.Net/Core/SampleReader.cs
@@ -392,12 +392,7 @@ namespace WavDotNet.Core
 
             for (var i = 0; i < length; i += 2)
             {
-                var a = bytes[i];
-                var a_ = a; //<< 16;
-                var b = bytes[i + 1];
-                var b_ = b << 8;
-                var c = a_ | b_;
-                tempSamples[i / 2] = (short)c;//(bytes[i] << 16 | bytes[i + 1] << 24);
+                tempSamples[i / 2] = (short)(bytes[i] | bytes[i + 1] << 8);
             }
 
             Marshal.FreeHGlobal((IntPtr)bytes);
