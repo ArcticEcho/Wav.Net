ff842765a7e0c7f27ddd1846306fa4be720e2f75
ProgramFOX <programfox@hotmail.be>
2015-02-11 16:07:58 +0100

    Add unit testing for Wav.Net
    This commit adds a small amount of unit tests for Wav.Net: it tests
    ToDecibels and ToAmplitude of the Math class. Travis integration is not
    implemented yet.

---
+++ Wav.Net.Tests/UnitTesting/TestMath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WavMath = WavDotNet.Tools.Math;
+
+namespace WavDotNet.Tests.UnitTesting
+{
+    [TestClass]
+    public class TestMath
+    {
+        [TestMethod]
+        public void TestToDecibels()
+        {
+            Assert.AreEqual(WavMath.ToDecibels(10D), 20);
+            Assert.AreEqual(WavMath.ToDecibels(10F), 20);
+            Assert.AreEqual(WavMath.ToDecibels(100D), 40);
+            Assert.AreEqual(WavMath.ToDecibels(100F), 40);
+        }
+
+        [TestMethod]
+        public void TestToAmplitude()
+        {
+            Assert.AreEqual(WavMath.ToAmplitude(100D), 100000);
+            Assert.AreEqual(WavMath.ToAmplitude(100F), 100000);
+        }
+    }
+}
